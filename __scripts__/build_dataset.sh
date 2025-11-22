a71a334e3f0e6f5aaa22e283992bdf2f36dc4852
Nefeli Paraskeuopoulou <nefeli.mnp@gmail.com>
2025-11-22 12:56:38 -0500

    fixed __scripts__

--- __scripts__/build_dataset.sh
+++ __scripts__/build_dataset.sh
@@ -1,4 +1,3 @@
-# Step 1: Convert tar.gz files to JSONL format
 python create_dataset/a_create_jsonl_from_tar.py oa_comm_xml.PMC000xxxxxx.baseline.2025-06-26.tar.gz
 python create_dataset/a_create_jsonl_from_tar.py oa_comm_xml.PMC001xxxxxx.baseline.2025-06-26.tar.gz 
 python create_dataset/a_create_jsonl_from_tar.py oa_comm_xml.PMC002xxxxxx.baseline.2025-06-26.tar.gz
@@ -12,6 +11,3 @@ python create_dataset/a_create_jsonl_from_tar.py oa_comm_xml.PMC009xxxxxx.baseli
 python create_dataset/a_create_jsonl_from_tar.py oa_comm_xml.PMC010xxxxxx.baseline.2025-06-26.tar.gz    
 python create_dataset/a_create_jsonl_from_tar.py oa_comm_xml.PMC011xxxxxx.baseline.2025-06-26.tar.gz
 python create_dataset/a_create_jsonl_from_tar.py oa_comm_xml.PMC012xxxxxx.baseline.2025-06-26.tar.gz
-
-# Step 2: Clean JSONL files. Keep only the records with non-empty 'abstract' and 'body_text' fields.
-python create_dataset/b_clean_jsonl.py
