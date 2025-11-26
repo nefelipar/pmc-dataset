e573d8c51dc3b8db5089bd53f3463c7240158561
Nefeli Paraskeuopoulou <nefeli.mnp@gmail.com>
2025-11-26 12:50:06 -0500

    ok

--- __scripts__/get_dataset_stats.sh
+++ __scripts__/get_dataset_stats.sh
@@ -5,6 +5,50 @@ python create_dataset/b_token_stats.py \
 python create_dataset/b_token_stats.py \
   --files "oa_comm_xml.PMC001xxxxxx.baseline.2025-06-26.jsonl" \
   --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC002xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC003xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC004xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC005xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC006xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &  
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC007xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC008xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC009xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC010xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC011xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
+
+python create_dataset/b_token_stats.py \
+  --files "oa_comm_xml.PMC012xxxxxx.baseline.2025-06-26.jsonl" \
+  --tokenizer "Qwen/Qwen1.5-1.8B" &
 wait
 
 
