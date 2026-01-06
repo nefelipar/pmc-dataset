404cd1ff60b793480818d0e08a2893184035f7e4
Nefeli Paraskeuopoulou <nefeli.mnp@gmail.com>
2026-01-07 00:43:14 +0200

    ok

--- __scripts__/create_dataset_random.sh
+++ __scripts__/create_dataset_random.sh
@@ -3,3 +3,17 @@ python utils/create_random_dataset.py \
   --output "__dataset__/random_500.jsonl" \
   --samples 500
 
+python utils/create_random_dataset.py \
+  --input "__dataset__/filtered_merged.jsonl" \
+  --output "__dataset__/random_1_000.jsonl" \
+  --samples 1000
+
+  python utils/create_random_dataset.py \
+  --input "__dataset__/filtered_merged.jsonl" \
+  --output "__dataset__/random_5_000.jsonl" \
+  --samples 5000
+
+  python utils/create_random_dataset.py \
+  --input "__dataset__/filtered_merged.jsonl" \
+  --output "__dataset__/random_10_000.jsonl" \
+  --samples 10000
